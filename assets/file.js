98abf90128b875ba2df191e2332e21ec81b0ba94
JuanManuelCabezaasContardo <juanmanuelcontardo@gmail.com>
2022-04-14 18:14:36 -0400

    feat():time image

--- assets/file.js
+++ assets/file.js
@@ -1,6 +1,7 @@
 // Set the date we're counting down to
 var countDownDate = new Date(config.Fecha).getTime();
 document.getElementById("Titulo").innerHTML = config.Titulo;
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -25,3 +26,27 @@ var x = setInterval(function() {
 		document.getElementById("demo").innerHTML = "Estamos Ready";
 	}
 }, 1000);
+
+
+var i = 0;
+var cambios = setInterval(function() {
+	
+	// background-image:url('img/1.gif');
+	document.body.style.backgroundImage=" url(\'img/1.gif\');";
+	
+	if((i % 3 ) == 1){
+		console.log(1);
+		document.body.style.backgroundImage="url('assets/img/1.gif')";
+	}
+	else if ((i % 3) == 2){
+		console.log(2);
+		document.body.style.backgroundImage="url('assets/img/2.gif')";
+	}
+	else if ((i % 3) == 0){
+		console.log(3);
+		document.body.style.backgroundImage="url('assets/img/3.gif')";
+	}
+	i = i +1;
+	/**/
+
+}, 60000);
